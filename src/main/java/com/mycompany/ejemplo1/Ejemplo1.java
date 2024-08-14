72f23529b8d4bb0605619949c27a4c9b1c2c0ca1
KALACAMAN <enriquepo96@gmail.com>
2024-08-13 18:25:14 -0600

    Se agrega versionamiento de Java

---
+++ src/main/java/com/mycompany/ejemplo1/Ejemplo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo1;
+
+/**
+ *
+ * @author Enrique Portillo
+ */
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
